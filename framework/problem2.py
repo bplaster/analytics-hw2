a473d87158e55b2894c2235b936d8e6e1eb14ef4
alapparikh <alap18@gmail.com>
2014-10-28 17:41:16 -0400

    unoptimized solution

--- framework/problem2.py
+++ framework/problem2.py
@@ -1,18 +1,20 @@
-import decision_tree
+import decision_tree as dt
 import scan
 import utils
 
 
 def main():
     binary_label = True
+    exclude_stopwords = True
 
-    data = scan.scan('foods.txt', binary_label)
+    data = scan.scan('finefoods.txt', exclude_stopwords, binary_label)
     length = len(data)
-
+    
     train_data = data[:int(length*.8)]
     test_data = data[int(length*.8):]
 
     decision_tree = dt.train(train_data)
+    #dt.check_tree(decision_tree)
     test_results = dt.test(decision_tree, test_data)
 
     print test_results
--- framework/utils.py
+++ framework/utils.py
@@ -1,9 +1,67 @@
-# decision tree stuff
-def entropy():
-    raise 'not implemented'
+from math import log
 
-def information_gain():
-    raise 'not implemented'
+# decision tree stuff
+def entropy(lst):
+    neg = 0
+    pos = 0
+
+    len_lst = len(lst)
+    if (len_lst == 0):
+        return 0
+
+    for ele in lst:
+        if ele == 0:
+            neg += 1
+        else:
+            pos += 1
+
+    prone = neg/float(len_lst)
+    przero = pos/float(len_lst)
+    #print prone
+    #print przero
+    if (prone == 0 or przero == 0):
+        return 0
+
+    entpy = -( prone*log(prone,2) + przero*log(przero,2) )
+    
+    return entpy
+
+def information_gain(data, attribute):
+    data_left = []
+    data_right = []
+    data_left,data_right = divide_dataset (data,attribute)
+
+    scores_list = get_scores(data)
+    scores_list_left = get_scores(data_left)
+    scores_list_right = get_scores(data_right)
+
+    len_data = len (scores_list)
+    len_data_left = len(scores_list_left)
+    len_data_right = len(scores_list_right)
+
+    infogain = entropy(scores_list) - (len_data_left/float(len_data))*entropy(scores_list_left) - (len_data_right/float(len_data))*entropy(scores_list_right)
+    
+    return infogain
+
+def divide_dataset(data,attribute):
+    data_left = []
+    data_right = []
+
+    for review in data:
+        words = review[0].split()
+        if attribute in words:
+            data_right.append(review)
+        else:
+            data_left.append(review)
+    
+    return data_left, data_right
+
+def get_scores(data):
+    scores_list = []
+    for ele in data:
+        scores_list.append(ele[1])
+    return scores_list
+
 
 # natural language processing stuff
 def freq(lst):

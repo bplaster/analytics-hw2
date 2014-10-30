cc2d32751a7f07fdeeb183ad8c4b6222075b380c
Brandon <baplaste@gmail.com>
2014-10-29 20:33:59 -0400

    Finally fixed A

--- program1/A.py
+++ program1/A.py
@@ -32,10 +32,15 @@ def derive_filter(rows,tolerance = 4.0):
     trip_dist_std = numpy.std(distances)
     logging.debug("Derived mean "+str(trip_dist_mean))
     logging.debug("Derived std "+str(trip_dist_std))
+    min_long = -74.2
+    max_long = -73.6
+    min_lat = 40.5
+    max_lat = 41.0
     def custom_filter(row):
         if row[1] != 0.0 and row[2] != 0.0 and row[3] != 0.0 and row[4] != 0.0 and row[5] != 0.0 and row[6] != 0.0 and row[7] != 0.0: # filters out rows with zero elements
             plong,plat,dlong,dlat=row[-4:]
-            if abs(plat) > tolerance and abs(dlat) > tolerance and abs(plong) > tolerance and abs(dlong) > tolerance:
+            # if plat > min_lat and plat and dlat > min_lat and plong > min_long and dlong > min_long:
+            if dlat > min_lat and dlat < max_lat and dlong > min_long and dlong < max_long:
                 if 100 > get_distance(plat,plong,dlat,dlong) > 0 and ((row[3] - trip_dist_mean) / trip_dist_std) < tolerance:
                     return True
         return False
@@ -51,8 +56,7 @@ def derive_bucket_transform(rows,indexes,div_size):
     min_values,max_values = {},{}
     first = True
     for row in rows:
-        row[0] = int(datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S" ).strftime("%s"))
-        for index in indexes:
+        for index in indexes[-2:]:
             if not first:
                 max_values[index] = max(max_values[index],row[index])
                 min_values[index] = min(min_values[index],row[index])
@@ -60,13 +64,13 @@ def derive_bucket_transform(rows,indexes,div_size):
                 max_values[index] = row[index]
                 min_values[index] = row[index]
         first = False
-    logging.debug("scale values min "+str(min_values))
-    logging.debug("scale values max "+str(max_values))
+    print "scale values min ", str(min_values)
+    print "scale values max ", str(max_values)
 
     def custom_transform(row):
         try:
-            row[0] = int(datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S" ).strftime("%s"))
             for index in indexes[-2:]:
+                # print row[index], min_values[index], max_values[index]
                 row[index] = int(round(div_size * (row[index] - min_values[index]) / (max_values[index]-min_values[index]),0))
             return row
         except:
@@ -74,12 +78,19 @@ def derive_bucket_transform(rows,indexes,div_size):
             raise ValueError
     return custom_transform
 
-def plot_buffer(buf, title):
+def plot_buffer(buf, title, n):
 
     # Plot
-    fig = plt.figure(title)
-    ax = Axes3D(fig)
-    ax.scatter(buf[:,0],buf[:,1],buf[:,2])
+    fig, ax = plt.subplots()
+    plt.title(title)
+    plt.xlabel('dropoff longitude (normalized)')
+    plt.ylabel('dropoff latitude (normalized)')
+    heatmap = ax.pcolor(buf, cmap="spectral")
+    plt.xlim(0,n)
+    plt.ylim(0,n)
+    cb = fig.colorbar(heatmap, ax=ax)
+    cb.set_label('probability density')
+
 
     # Clear the buffer
     buf = []
@@ -90,7 +101,7 @@ if __name__ == '__main__':
     models = []
     features = (4,5,6,7) 
     target = 1
-    div_sz = 100
+    div_sz = 200
     max_pass = 7
 
     #curr_data = EXAMPLE_DATA
@@ -103,31 +114,31 @@ if __name__ == '__main__':
     bucket_transform = derive_bucket_transform(utils.load_csv_lazy(curr_data,S_FIELDS,F_FIELDS,row_filter=mean_dev_filter),features, div_sz)
 
     # Load Train data
-    train_data = utils.load_csv_lazy(curr_data, S_FIELDS, F_FIELDS, row_filter = mean_dev_filter, row_transformer = bucket_transform)
+    train_data = [row for row in utils.load_csv_lazy(curr_data,S_FIELDS,F_FIELDS, row_filter = mean_dev_filter, row_transformer = bucket_transform)]
 
     # Create empty containers
     raw_buckets = [[[0 for k in xrange(max_pass+1)] for j in xrange(div_sz+1)] for i in xrange(div_sz+1)]
-    density_buckets = []
+    density_buckets = [[[0 for k in xrange(max_pass+1)] for j in xrange(div_sz+1)] for i in xrange(div_sz+1)]
 
     # Determine bucket counts
     for i,row in enumerate(train_data):
+        # if i == 10**3:
+        #     break
         raw_buckets[row[features[2]]][row[features[3]]][int(row[target])]+=1
 
-    # Determine densities
+     # Determine densities
     for k in range(max_pass):
-        density_buckets.append([])
         for i in xrange(div_sz):
             for j in xrange(div_sz):
                 total = sum(raw_buckets[i][j][:])
                 if total:
                     density = raw_buckets[i][j][k]/total
-                    (density_buckets[k]).append([i,j,density])
+                    density_buckets[i][j][k] = density
 
-    print "Total points plotted: ", len(density_buckets[1])
-    # # Plot data
-    # plot_buffer(numpy.array(density_buckets[1]),'1 Person')
-    # plot_buffer(numpy.array(density_buckets[3]),'3 Person')
-    # plt.show()
+    # Plot data
+    plot_buffer(numpy.array(density_buckets)[:,:,1],'Probability density of 1 Passenger, given dropoff lat, long', div_sz)
+    plot_buffer(numpy.array(density_buckets)[:,:,3],'Probability density of 3 Passengers, given dropoff lat, long', div_sz)
+    plt.show()
 
 
     # clear the buffer
